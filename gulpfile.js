c824a390524efe3399f65b2cda310de909e5ce54
alanmcginnis <amwebtools@gmail.com>
2018-12-29 17:55:43 -0800

    🔨Laying the foundation.
    Back to Gulp because it already does what I need it to.

---
+++ gulpfile.js
@@ -0,0 +1,120 @@
+const gulp = require('gulp');
+const plugins = require('gulp-load-plugins')();
+
+// Path Constants
+
+// Source Files
+const Paths = {
+	SCSS_SRC: 'source/css/master.scss',
+	JS_SRC: 'source/js/master.js',
+	HBARS_SRC: 'source/js/vendor/handlebars*.js',
+	SVG_SRC: 'source/media/*.svg',
+	CSS_DEST: 'css',
+	JS_DEST: 'js',
+	SVG_DEST: 'media'
+};
+
+// Destination File names
+const outFileNames = {
+	CSS: 'master.min.css',
+	JS: 'master.min.js'
+};
+
+// Watch paths
+const watchPaths = {
+	SCSS: 'source/css/**/*.scss',
+	JS: 'source/js/**/*.js',
+	SVG: 'source/media/**/*.svg',
+};
+
+// CSS/SCSS Scripts
+
+gulp.task('build-scss', function () {
+	return gulp.src(Paths.SCSS_SRC)
+		.pipe(plugins.plumber({
+			errorHandler: function (err) {
+				plugins.notify.onError({
+					title: 'SCSS Build Error',
+					message: err.message
+				})(err);
+			}
+		}))
+		.pipe(plugins.sourcemaps.init())
+		.pipe(plugins.sass())
+		.on('error', plugins.sass.logError)
+		.pipe(plugins.autoprefixer({
+			browsers: ['last 2 versions', 'ie >= 11']
+		}))
+		.pipe(plugins.cleanCss({
+			keepSpecialComments: 0
+		}))
+		.pipe(plugins.rename(outFileNames.CSS))
+		.pipe(plugins.sourcemaps.write('.'))
+		.pipe(gulp.dest(Paths.CSS_DEST))
+		.on('error', plugins.notify.onError({
+			message: '<%= error.message %>',
+			title: 'SCSS Error'
+		}));
+});
+
+// JS Scripts
+
+gulp.task('build-js', function () {
+	return gulp.src([srcFiles.jquery, srcFiles.handlebars, srcFiles.js])
+		.pipe(plugins.plumber({
+			errorHandler: function (err) {
+				plugins.notify.onError({
+					title: 'JS Build Error',
+					message: err.message
+				})(err);
+			}
+		}))
+		.pipe(plugins.notify({
+			message: 'Starting JS build',
+			title: 'JS Build'
+		}))
+		.pipe(plugins.uglify())
+		.pipe(plugins.concat(outFileNames.JS))
+		.pipe(gulp.dest(Paths.JS_DEST));
+});
+
+// Sprite Generation
+
+var spritesConfig = {
+	preview: false,
+	svg: {
+		sprite: "media/sprite.svg"
+	},
+	cssFile: "../source/css/sass-includes/_sprite.scss",
+};
+
+gulp.task('build-sprites', function () {
+	return gulp.src(srcFiles.svgs)
+		.pipe(plugins.svgSprites(spritesConfig))
+		.pipe(gulp.dest('./'));
+});
+
+// Build Scripts and Defaults
+
+gulp.task('build', gulp.parallel('build-scss', 'build-js', 'build-sprites'));
+
+gulp.task('watch-sprites', function () {
+	gulp.watch(watchPaths.SVG, gulp.parallel('build-sprites'));
+});
+
+gulp.task('watch-source-scss', function () {
+	gulp.watch(watchPaths.SCSS, gulp.parallel('build-scss'));
+});
+
+gulp.task('watch-source-js', function () {
+	gulp.watch(watchPaths.JS, gulp.parallel('build-js'));
+});
+
+gulp.task('watch', gulp.series(
+	gulp.parallel('watch-source-scss', 'watch-source-js', 'watch-sprites')
+));
+
+gulp.task('default', gulp.series(
+	gulp.parallel('build'),
+	gulp.parallel('watch')
+));
